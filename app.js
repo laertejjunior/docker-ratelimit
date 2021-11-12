f13bfe08b43f6e8572295ffbe0ac8ef6bea1d015
ljamjunior <laertejjunior@gmail.com>
2021-11-12 12:34:22 -0300

    remove token file

--- app.js
+++ app.js
@@ -5,99 +5,116 @@ const yargs = require("yargs");
 const inquirer = require("inquirer");
 const package = require("./package.json");
 const rateLimit = require("./util/ratelimit");
+const ora = require("ora");
 
 const version = package["version"];
 
 yargs.version(version);
 
 const getAuth = async () => {
-    const auth = await inquirer
-        .prompt([
-            {
-                type: "input",
-                name: "username",
-                message: "username",
-            },
-            {
-                type: "password",
-                message: "password",
-                name: "password",
-                mask: "*",
-            },
-        ]);
-
-    return auth;
-}
-
-yargs.command("$0", "Show docker pull rate limit", {
-    builder: {
-        "proxy.host": {
-            describe: "Proxy host",
-            type: "string",
-        },
-        "proxy.port": {
-            describe: "Proxy port",
-            type: "string",
-        },
-        authenticated: {
-            describe: "Verify authenticated request limits",
-            type: "boolean",
-        },
-        raw: {
-            describe: "show raw head return",
-            type: "boolean",
-        },
+  const auth = await inquirer.prompt([
+    {
+      type: "input",
+      name: "username",
+      message: "username",
     },
-    handler: async (argv) => {
-        auth = null;
-
-        if (argv.authenticated) {
-            auth = await getAuth();
-        }
-
-        rateLimit.checkRateLimit(auth, argv).then((data) => {
-            if (data.error) {
-                console.log(data.error);
-            } else if (argv.raw) {
-                console.log(data);
-            } else {
-                const ratelimitRemaining = data["ratelimit-remaining"].split(";")[0];
-                const ratelimitLimit = data["ratelimit-limit"].split(";")[0];
-                const limitHours = data["ratelimit-limit"].split("=")[1] / 3600;
-                const dots = 20;
-
-                let remaining = "Remaining";
-                let date = "Date";
-                let limit = "Limit";
-                let source = "Source";
-
-                if (ratelimitRemaining > 0) {
-                    remaining = chalk.greenBright(
-                        `${remaining.padEnd(dots, ".")}: ${ratelimitRemaining}`
-                    );
-                } else {
-                    remaining = chalk.red(
-                        `${remaining.padEnd(dots, ".")}: ${ratelimitRemaining}`
-                    );
-                }
-
-                date = chalk.whiteBright(`${date.padEnd(dots, ".")}: ${data["date"]}`);
-
-                limit = chalk.whiteBright(
-                    `${limit.padEnd(dots, ".")}: ${ratelimitLimit}/${limitHours}h`
-                );
-
-                source = chalk.whiteBright(
-                    `${source.padEnd(dots, ".")}: ${data["docker-ratelimit-source"]}`
-                );
-
-                console.log(date);
-                console.log(limit);
-                console.log(remaining);
-                console.log(source);
-            }
-        });
+    {
+      type: "password",
+      message: "password",
+      name: "password",
+      mask: "*",
     },
-});
+  ]);
+
+  return auth;
+};
+
+yargs
+  .scriptName("docker-ratelimit")
+  .usage("$0 [args]")
+  .command(
+    "$0",
+    "Show docker pull rate limit",
+    {
+      "proxy.host": {
+        alias: 'h',
+        describe: "Proxy host",
+        type: "string",
+      },
+      "proxy.port": {
+        alias: 'p',
+        describe: "Proxy port",
+        type: "string",
+      },
+      authenticated: {
+        alias: 'a',
+        describe: "Verify authenticated request limits",
+        type: "boolean",
+      },
+      raw: {
+        alias: 'r',
+        describe: 'show raw head return',
+        type: 'boolean',
+      },
+    },
+    async (argv) => {
+      auth = null;
+
+      if (argv.authenticated) {
+        auth = await getAuth();
+      }
+      console.clear();
+
+      showResult(auth, argv);
+    }
+  );
+
+const showResult = (auth, argv) => {
+  const spinner = ora("Loading data...").start();
+  rateLimit.checkRateLimit(auth, argv).then((data) => {
+    if (data.error) {
+      spinner.fail(data.error);
+    } else if (argv.raw) {
+      spinner.succeed("Done");
+      console.log(data);
+    } else {
+      const ratelimitRemaining = data["ratelimit-remaining"].split(";")[0];
+      const ratelimitLimit = data["ratelimit-limit"].split(";")[0];
+      const limitHours = data["ratelimit-limit"].split("=")[1] / 3600;
+      const dots = 20;
+
+      let remaining = "Remaining";
+      let date = "Date";
+      let limit = "Limit";
+      let source = "Source";
+
+      if (ratelimitRemaining > 0) {
+        remaining = chalk.greenBright(
+          `${remaining.padEnd(dots, ".")}: ${ratelimitRemaining}`
+        );
+      } else {
+        remaining = chalk.red(
+          `${remaining.padEnd(dots, ".")}: ${ratelimitRemaining}`
+        );
+      }
+
+      date = chalk.whiteBright(`${date.padEnd(dots, ".")}: ${data["date"]}`);
+
+      limit = chalk.whiteBright(
+        `${limit.padEnd(dots, ".")}: ${ratelimitLimit}/${limitHours}h`
+      );
+
+      source = chalk.whiteBright(
+        `${source.padEnd(dots, ".")}: ${data["docker-ratelimit-source"]}`
+      );
+
+      spinner.succeed("Done");
+      console.log(date);
+      console.log(limit);
+      console.log(remaining);
+      console.log(source);
+    }
+  });
+};
 
 yargs.parse();
--- util/ratelimit.js
+++ util/ratelimit.js
@@ -1,14 +1,9 @@
 #!/usr/bin/env node
 
-const fs = require("fs");
-const moment = require("moment");
 const package = require("../package.json");
 const HttpsProxyAgent = require('https-proxy-agent');
-
 const url = package.config["token-url"];
 const validationUrl = package.config["validation-url"];
-const fileName = package.config["file-name"];
-const expiresIn = package.config["expires-in"];
 
 var axios = null;
 
@@ -29,6 +24,27 @@ const checkRateLimit = async (auth, argv) => {
         },
     };
 
+    const rateLimitInfo = await getRaleLimitInfo(config);
+
+    return rateLimitInfo;
+};
+
+const getToken = async (auth) => {
+    try {
+        const response = await axios.get(url, { auth });
+
+        return {
+            token: response.data.token,
+        };
+    } catch (error) {
+        const data = {
+            error: getErrorMessage(error),
+        };
+        return data;
+    }
+};
+
+const getRaleLimitInfo = async (config) => {
     try {
         const response = await axios.head(validationUrl, config);
 
@@ -39,65 +55,6 @@ const checkRateLimit = async (auth, argv) => {
         };
 
         return data;
-    }
-};
-
-const getErrorMessage = (err) => {
-    if (err.response) {
-        return `${err.response.status}: ${err.response.statusText}`;
-    } else if (err.request) {
-        return err.message;
-    } else {
-        return err.message;
-    }
-};
-
-const getToken = async (auth) => {
-    let now = moment();
-    let issuedAt = null;
-    let minutes = null;
-
-    const token = loadToken();
-    if (token) {
-        issuedAt = moment(token.issued_at);
-        minutes = now.diff(issuedAt, "seconds");
-    }
-
-    if (!auth && token && minutes < expiresIn) {
-        return {
-            token: token.token,
-        };
-    } else {
-        try {
-            const response = await axios.get(url, { auth });
-
-            if (!auth) {
-                saveToken(response.data);
-            }
-            return {
-                token: response.data.token,
-            };
-        } catch (error) {
-            const data = {
-                error: getErrorMessage(error),
-            };
-            return data;
-        }
-    }
-};
-
-const saveToken = (token) => {
-    const jsonString = JSON.stringify(token);
-    fs.writeFileSync(fileName, jsonString);
-};
-
-const loadToken = () => {
-    try {
-        const tokenBuffer = fs.readFileSync(fileName);
-
-        return JSON.parse(tokenBuffer.toString());
-    } catch (error) {
-        return null;
     }
 };
 
@@ -112,6 +69,17 @@ const getProxyConfig = (proxy) => {
         httpsAgent: new HttpsProxyAgent(`${proxy.host}:${proxy.port}`)
     };
 }
+
+const getErrorMessage = (err) => {
+    if (err.response) {
+        return `${err.response.status}: ${err.response.statusText}`;
+    } else if (err.request) {
+        return err.message;
+    } else {
+        return err.message;
+    }
+};
+
 
 module.exports = {
     checkRateLimit,
